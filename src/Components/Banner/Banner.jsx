aded7c791249d588c0eb788f2acf3a96781a5025
ArnabNayan <arnabnayan5@gmail.com>
2023-05-18 18:01:16 +0600

    banner created

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-           <h2>This is banner</h2> 
-        </div>
+     <>
+           <div
+                className="bg-cover backdrop-brightness-20 bg-center h-screen w-full"
+                style={{
+                    backgroundImage:
+                        "url('https://www.lego.com/cdn/cs/set/assets/bltcc14d63e84cdc719/01_76190_Banner_tall_d.jpg?fit=crop&format=jpg&quality=80&width=1600&height=700&dpr=1')",
+                }}
+            >  
+            <div className="w-full h-full items-center left-0  top-0 bg-gradient-to-r from-[black] to-[gray) 100%">
+                
+                
+
+               <div className='pt-40'> 
+               <h1 className='flex justify-start text-3xl lg:text-6xl ms-8 text-orange-500 font-bold '>Welcome to Kidsfun </h1>
+                <h1 className='flex justify-start text-3xl lg:text-4xl ms-8 mt-10 text-blue-200 font-bold '>Get The Best Toy For <br /> Your Kid </h1></div>
+                <p className='text-xl text-yellow-400  mt-5 font-serif ms-8'>Make playtime enjoyable with our  finest toys and games</p>
+                </div>
+            </div>
+    </>
     );
 };
 
--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
+
 const Gallery = () => {
     return (
-        <div>
-            <h2>This is gallery</h2>
-        </div>
-    );
+       <>
+          <h2 className='text-5xl mt-14 text-indigo-950 font-bold'>Our Photo Gallery</h2> 
+          <div className="h-96 carousel carousel-vertical rounded-box">
+  <div className="carousel-item h-full">
+    <img src="https://www.fnp.com/images/pr/x/v20200817204537/kids-avengers-action-figure-toy-spiderman-red_5.jpg" />
+    <img src="https://cdn.shopify.com/s/files/1/0064/6679/7632/articles/Genuine-Marvel-Super-Heroes-Avengers-Endgame-Iron-Man-Hulk-Captain-America-Spider-Man-Action-Figure-Model_480x540_crop_center.png?v=1618590731" />
+    <img src="" />
+    <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  </div> 
+  <div className="carousel-item h-full">
+  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  </div> 
+  <div className="carousel-item h-full">
+  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+    
+  </div> 
+  
+</div>
+        
+{/* <div className="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" className="btn btn-xs">1</a> 
+  <a href="#item2" className="btn btn-xs">2</a> 
+  <a href="#item3" className="btn btn-xs">3</a> 
+  <a href="#item4" className="btn btn-xs">4</a>
+</div>  */}
+       </>
+  );
 };
+
+    
+ 
 
 export default Gallery;
