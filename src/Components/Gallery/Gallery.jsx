1f01a41c15eba87303fa9c45845a18c64f5d5bb0
ArnabNayan <arnabnayan5@gmail.com>
2023-05-18 23:04:51 +0600

    login file created

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -27,13 +27,6 @@ const Gallery = () => {
   </div> 
   
 </div>
-        
-{/* <div className="flex justify-center w-full py-2 gap-2">
-  <a href="#item1" className="btn btn-xs">1</a> 
-  <a href="#item2" className="btn btn-xs">2</a> 
-  <a href="#item3" className="btn btn-xs">3</a> 
-  <a href="#item4" className="btn btn-xs">4</a>
-</div>  */}
        </>
   );
 };
--- src/Components/Layout/Login/Login.jsx
+++ src/Components/Layout/Login/Login.jsx
@@ -2,9 +2,41 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="w-1/2 lg:ml-20">
+             <img className='h-2/4 w-3/4' src="https://img.freepik.com/free-vector/login-concept-illustration_114360-739.jpg?w=2000" alt="" />
+           
+          </div>
+       
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        
+            <div className="card-body">
+            <h1 className="text-5xl font-bold text-center text-orange-500">Login now!</h1>
+            <form>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="text" placeholder="email" className="input input-bordered" />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="text" placeholder="password" className="input input-bordered" />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+              <input className="btn btn-warning" type="submit"value="Login" />
+              </div>
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
