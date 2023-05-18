5653b5b998e3f131dca5fdf391b72bd978e471b8
ArnabNayan <arnabnayan5@gmail.com>
2023-05-18 18:46:47 +0600

    photo gallery section created

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
      <>
            <div
-                className="bg-cover backdrop-brightness-20 bg-center h-screen w-full"
+                className="bg-cover backdrop-brightness-20 bg-center h-64 lg:h-screen w-full "
                 style={{
                     backgroundImage:
                         "url('https://www.lego.com/cdn/cs/set/assets/bltcc14d63e84cdc719/01_76190_Banner_tall_d.jpg?fit=crop&format=jpg&quality=80&width=1600&height=700&dpr=1')",
@@ -14,10 +14,10 @@ const Banner = () => {
                 
                 
 
-               <div className='pt-40'> 
-               <h1 className='flex justify-start text-3xl lg:text-6xl ms-8 text-orange-500 font-bold '>Welcome to Kidsfun </h1>
-                <h1 className='flex justify-start text-3xl lg:text-4xl ms-8 mt-10 text-blue-200 font-bold '>Get The Best Toy For <br /> Your Kid </h1></div>
-                <p className='text-xl text-yellow-400  mt-5 font-serif ms-8'>Make playtime enjoyable with our  finest toys and games</p>
+               <div className='lg:pt-40 pt-10'> 
+               <h1 className='flex justify-start text-xl lg:text-6xl lg:ms-8 text-orange-500 font-bold '>Welcome to Kidsfun </h1>
+                <h1 className='flex justify-start text-lg lg:text-4xl lg:ms-8 lg:mt-10 text-blue-200 font-bold '>Get The Best Toy For <br /> Your Kid </h1></div>
+                <p className='text-lg lg:text-xl text-yellow-400  lg:mt-5 font-serif lg:ms-8'>Make playtime enjoyable with our  finest toys and games</p>
                 </div>
             </div>
     </>
--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -4,25 +4,25 @@ import React from 'react';
 const Gallery = () => {
     return (
        <>
-          <h2 className='text-5xl mt-14 text-indigo-950 font-bold'>Our Photo Gallery</h2> 
-          <div className="h-96 carousel carousel-vertical rounded-box">
-  <div className="carousel-item h-full">
+          <h2 className='text-5xl mt-14 text-indigo-950 font-bold text-center'>Our Photo Gallery</h2> 
+          <div className="h-96 carousel carousel-horizontal lg:carousel-vertical  rounded-box mt-8 bg-slate-300">
+  <div className="carousel-item h-full gap-4">
     <img src="https://www.fnp.com/images/pr/x/v20200817204537/kids-avengers-action-figure-toy-spiderman-red_5.jpg" />
     <img src="https://cdn.shopify.com/s/files/1/0064/6679/7632/articles/Genuine-Marvel-Super-Heroes-Avengers-Endgame-Iron-Man-Hulk-Captain-America-Spider-Man-Action-Figure-Model_480x540_crop_center.png?v=1618590731" />
-    <img src="" />
-    <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDGiHzf15VBY1iVggaUUzzAH84RBsNjCXCJw&usqp=CAU" />
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4KzMAMaKjvm98UcFaWGUQ9e7UNDehk0J_Jg&usqp=CAU" />
   </div> 
-  <div className="carousel-item h-full">
-  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
-  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
-  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
-  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  <div className="carousel-item h-full mt-7 gap-4">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRutw-5VkVEKZoALox9C9-7aitH53zEkRr5gg&usqp=CAU" />
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZLeu4n6U9XAraVumxYPdBNg3R_A1qp6IX1A&usqp=CAU" />
+  <img src="https://i1.fnp.com/images/pr/l/v20200817204535/kids-avengers-action-figure-toy-spiderman-red_1.jpg" />
+  <img src="https://i.ebayimg.com/images/g/Q6cAAOSwRKhjvjCu/s-l1600.png" />
   </div> 
-  <div className="carousel-item h-full">
-  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
-  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
-  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
-  <img src="https://static-01.daraz.com.bd/p/814ba6e5bcad576c38935410c09ba3ae.jpg_720x720.jpg_.webp" />
+  <div className="carousel-item h-full mt-7 gap-4">
+  <img src="https://m.media-amazon.com/images/I/610PMc2t+lL._AC._SR360,460.jpg" />
+  <img src="https://m.media-amazon.com/images/I/411wMdcE27L._AC_.jpg" />
+  <img src="https://m.media-amazon.com/images/I/41V6kMej9cL._AC_SY350_.jpg" />
+  <img src="https://m.media-amazon.com/images/I/811K7khoBLL._AC_SY355_.jpg" />
     
   </div> 
   
