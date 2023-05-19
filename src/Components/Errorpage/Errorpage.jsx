d87fdd4af4e9c14a9290fd320fa8cf835f491136
ArnabNayan <arnabnayan5@gmail.com>
2023-05-19 21:42:23 +0600

    404 errorpage done

--- src/Components/Errorpage/Errorpage.jsx
+++ src/Components/Errorpage/Errorpage.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Errorpage = () => {
     return (
         <div>
-              {/* <h1 className='text-center font-bold text-6xl'>Oops!Page not found</h1>
-           <img className='w-2/3 mx-auto h-full' src="https://st2.depositphotos.com/1431107/9115/v/950/depositphotos_91153822-stock-illustration-404-error-sign.jpg" alt="" />
-           <p className='text-center font-bold text-6xl'>We are sorry,the page you requested is not present here</p>  */}
+              <h1 className='text-center font-bold text-6xl'>Sorry!!!!!!!!!!!</h1>
+           <img className='w-2/3 mx-auto h-full' src="https://img.freepik.com/premium-vector/404-error-page-found-vector-with-ufo_114341-146.jpg" alt="" />
+           <p className='text-center font-bold text-6xl'>You are in the wrong site</p> 
 
         </div>
     );
