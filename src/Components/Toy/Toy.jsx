56a73894be99cfae4ac8f9317fd61f99b28ff858
ArnabNayan <arnabnayan5@gmail.com>
2023-05-21 01:53:10 +0600

    data showed by the subcategory

--- src/Components/Toy/Toy.jsx
+++ src/Components/Toy/Toy.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const Toy = () => {
+// eslint-disable-next-line react/prop-types
+const Toy = ({toy}) => {
+    // eslint-disable-next-line react/prop-types
+  const {name,photo,price,rating}=toy
     return (
-        <div>
-            
-        </div>
+        <div className="hero bg-base-200">
+                  <div className="hero-content flex-col lg:flex-row">
+                    <img src={photo} className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                      <h1 className="text-3xl font-bold">{name}</h1>
+                      <p className='py-4'>Price:{price}</p>
+                      <p>Rating:{rating}</p>
+                      <button className="btn btn-secondary mt-4">View Details</button>
+                    </div>
+                  </div>
+                </div>
     );
 };
 
